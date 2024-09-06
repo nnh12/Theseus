dbdcf060b7689687ac85baa8e1d84f9be0566df4
Nathan Hsiao <nnh12@DESKTOP-EQ10QSJ>
2024-09-06 16:22:11 -0400

    Remove extra line

--- applications/cd/src/lib.rs
+++ applications/cd/src/lib.rs
@@ -37,7 +37,6 @@ pub fn main(args: Vec<String>) -> isize {
         curr_env.lock().working_dir = Arc::clone(root::get_root());
     } else {
         let path = matches.free[0].as_ref();
-        
         match curr_env.lock().chdir(path) {
             Err(environment::Error::NotADirectory) => {
                 println!("not a directory: {}", path);
