f6a11c823407fc11699affb6ecad6d8ad9ea9136
nathanhsiao123@gmail.com <nathanhsiao123@gmail.com>
2024-12-17 00:07:39 -0500

    initial implementation for first come first serve

--- kernel/scheduler_fcfs/src/lib.rs
+++ kernel/scheduler_fcfs/src/lib.rs
@@ -1,21 +1,14 @@
-//! This crate picks the next task in round robin fashion.
-//! Each time the task at the front of the queue is picked.
-//! This task is then moved to the back of the queue.
-
-#![no_std]
-
 extern crate alloc;
 
-use alloc::{boxed::Box, collections::VecDeque, vec::Vec};
-
+use alloc::collections::VecDeque;
 use task::TaskRef;
 
-pub struct Scheduler {
+pub struct FCFSScheduler {
     idle_task: TaskRef,
     queue: VecDeque<TaskRef>,
 }
 
-impl Scheduler {
+impl FCFSScheduler {
     pub const fn new(idle_task: TaskRef) -> Self {
         Self {
             idle_task,
@@ -24,41 +17,27 @@ impl Scheduler {
     }
 }
 
-impl task::scheduler::Scheduler for Scheduler {
+impl task::scheduler::Scheduler for FCFSScheduler {
     fn next(&mut self) -> TaskRef {
-        if let Some((task_index, _)) = self
-            .queue
-            .iter()
-            .enumerate()
-            .find(|(_, task)| task.is_runnable())
-        {
-            let task = self.queue.swap_remove_front(task_index).unwrap();
-            self.queue.push_back(task.clone());
+        if let Some(task) = self.queue.pop_front() {
             task
         } else {
+            // Return an idle task if no other task is available
             self.idle_task.clone()
         }
-    }
-
-    fn busyness(&self) -> usize {
-        self.queue.len()
     }
 
     fn add(&mut self, task: TaskRef) {
         self.queue.push_back(task);
     }
 
-    fn remove(&mut self, task: &TaskRef) -> bool {
-        let mut task_index = None;
-        for (i, t) in self.queue.iter().enumerate() {
-            if t == task {
-                task_index = Some(i);
-                break;
-            }
-        }
+    fn busyness(&self) -> usize {
+        self.queue.len()
+    }
 
-        if let Some(task_index) = task_index {
-            self.queue.remove(task_index);
+    fn remove(&mut self, task: &TaskRef) -> bool {
+        if let Some(pos) = self.queue.iter().position(|t| t == task) {
+            self.queue.remove(pos);
             true
         } else {
             false
